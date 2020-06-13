c3f72c871a48e4f6e37c6800aa7e27c080f46f91
ntan <nguyenthanhan1001@gmail.com>
2020-06-13 11:09:53 +0700

    update face detection

--- face_detection/face_det.py
+++ face_detection/face_det.py
@@ -1,14 +1,28 @@
 import cv2
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(0)
+	cap = cv2.VideoCapture(0)
+	face_cascade = cv2.CascadeClassifier('cvdata/haarcascade_frontalface_default.xml')
 
-    while(True):
-        ret, frame = cap.read()
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    cap.release()
-    cv2.destroyAllWindows()
-    pass
+	while(True):
+		ret, img = cap.read()
+
+		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		faces = face_cascade.detectMultiScale(
+			gray,
+			scaleFactor=1.1,
+			minNeighbors=5,
+			minSize=(30, 30),
+			flags=cv2.CASCADE_SCALE_IMAGE
+		)
+
+		for x, y, w, h in faces:
+			img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+		cv2.imshow('frame',img)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+	
+	cap.release()
+	cv2.destroyAllWindows()
+	pass
